5986b7dcb4ab1b571cdd1096161ec6fcae44ed08
onyoo <roberto9m9@gmail.com>
2016-03-13 17:35:50 +0100

    require login to view posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :require_user, only: [:show, :new, :create]
 
   def index
     @posts = Post.order(created_at: :desc).page(params[:page])
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   resources :users
   resources :comments
   resources :replies, only: [:create]
-  resources :sessions, only: [:new, :create, :destroy]
+  
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
