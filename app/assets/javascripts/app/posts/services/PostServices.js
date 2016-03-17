bdd99b18d4eb730c51e10913b1f8fe3e369f4b52
onyoo <roberto9m9@gmail.com>
2016-03-17 23:31:04 +0100

    using  instead

--- app/assets/javascripts/app/posts/services/PostServices.js
+++ app/assets/javascripts/app/posts/services/PostServices.js
@@ -1,12 +1,38 @@
-function PostServices($resource) {
+function PostServices($http) {
 
-  this.posts = [{
-    title: "this is a post from the controller",
-    body: "This is a body from the controller"
-  }, {
-    title: "This is another from the controller",
-    body: "Number 2 from the controller"
-  }];
+  this.posts = $http({
+        method: 'GET',
+        url: 'http://localhost:3000/posts.json'
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+
+debugger;
+//     var posts = {
+//   	getPost: function(){
+//     	return $http({
+//         	method: 'GET',
+//         	url: 'http://localhost:3000/posts.json'
+//     	})
+//     	.then(function (r) {
+//         	posts.post = r.data;
+//         	console.log(posts.post);
+//     	});
+// 	};
+// ​
+// 	posts.getPost();
+// ​
+// 	return posts;
+
+
+  // [{
+  //   title: "this is a post from the controller",
+  //   body: "This is a body from the controller"
+  // }, {
+  //   title: "This is another from the controller",
+  //   body: "Number 2 from the controller"
+  // }];
 
 }
 
